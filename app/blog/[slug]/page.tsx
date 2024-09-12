5722bf3011452bafe0019b0cab0669b558a9f2f1
Colum Kelly <columk1@users.noreply.github.com>
2024-09-11 22:58:44 -0700

    Add markup for main blog post page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,15 +1,45 @@
 import dynamic from 'next/dynamic'
 import { getAllBlogPostsData } from '@/lib/getAllBlogPostsData'
+import { getBlogPostMetadata } from '@/lib/getBlogPostMetadata'
+import SquaresIcon from '@/ui/icons/SquaresIcon'
 
 type BlogPageProps = { params: { slug: string } }
 
-export default function BlogPage({ params }: BlogPageProps) {
+export default async function BlogPage({ params }: BlogPageProps) {
+  const { metadata } = await getBlogPostMetadata(params.slug)
   const BlogMarkdown = dynamic(() => import(`@/content/${params.slug}.mdx`))
+
   return (
-    <div className='container mx-auto p-4'>
-      <h2 className='text-xl'>{params.slug}</h2>
-      <BlogMarkdown />
-    </div>
+    <>
+      <h2 className={'mx-0 my-4 text-4xl'}>{metadata.title}</h2>
+      <div className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-1'>
+          <p className={'font-detail text-xs'}>
+            {new Date(metadata.date).toLocaleDateString('en-US', {
+              month: 'short',
+              day: 'numeric',
+              year: 'numeric',
+            })}
+          </p>
+          <div className='flex gap-2 font-detail text-xs'>
+            {metadata.tags && <p>{metadata.tags?.map((tag) => tag.toUpperCase()).join(', ')}</p>}
+            {metadata.readingLength && (
+              <>
+                <p>|</p>
+                <p>{`${metadata.readingLength} MIN READ`}</p>
+              </>
+            )}
+          </div>
+        </div>
+        <div className='h-5 w-full bg-text-primary' />
+        <div className='my-2'>
+          <SquaresIcon styles={{ width: '40px', color: 'var(--accent)' }} />
+        </div>
+      </div>
+      <div className={'markdown'}>
+        <BlogMarkdown />
+      </div>
+    </>
   )
 }
 
