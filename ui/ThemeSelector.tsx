2f279cc8a90495825b5df5cf47b8ac7d6fea9c24
Colum Kelly <columk1@users.noreply.github.com>
2024-09-23 18:09:40 -0700

    Fix: Theme change slow performance

--- ui/ThemeSelector.tsx
+++ ui/ThemeSelector.tsx
@@ -1,27 +1,30 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { setThemeCookie } from '@/app/actions'
 
-type Theme = 'light' | 'dark' | null
+type Theme = 'light' | 'dark'
 
 const ThemeSelector = ({ initialValue }: { initialValue: Theme }) => {
   const [theme, setTheme] = useState<Theme>(initialValue)
 
+  const handleThemeChange = useCallback((theme: Theme) => {
+    setTheme(theme)
+    setThemeCookie(theme)
+    document.body.classList.toggle('dark', theme === 'dark')
+  }, [])
+
   useEffect(() => {
-    if (theme) {
-      setThemeCookie(theme)
-      document.body.classList.toggle('dark', theme === 'dark')
-    } else {
+    if (!theme) {
       const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-      setTheme(prefersDark ? 'dark' : 'light')
+      handleThemeChange(prefersDark ? 'dark' : 'light')
     }
-  }, [theme])
+  }, [theme, handleThemeChange])
 
   return (
     <div className='-rotate-90 absolute bottom-[--frame] left-[2.65rem] origin-bottom-left text-xs'>
       <ul className='flex gap-3'>
-        <button type='button' onClick={() => setTheme('light')}>
+        <button type='button' onClick={() => handleThemeChange('light')}>
           <li className='flex items-center gap-1 font-light font-sans hover:[-webkit-text-stroke:0.5px]'>
             {theme === 'light' ? (
               <span className='pb-1 text-base leading-7'>■</span>
@@ -31,7 +34,7 @@ const ThemeSelector = ({ initialValue }: { initialValue: Theme }) => {
             LIGHT
           </li>
         </button>
-        <button type='button' onClick={() => setTheme('dark')}>
+        <button type='button' onClick={() => handleThemeChange('dark')}>
           <li className='flex items-center gap-1 font-light font-sans hover:[-webkit-text-stroke:0.5px]'>
             {theme === 'dark' ? (
               <span className='pb-1 text-base leading-7'>■</span>
