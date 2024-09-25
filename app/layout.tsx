a6f4e9057fee8f188f935200c507662bb80539d4
Colum Kelly <columk1@users.noreply.github.com>
2024-09-25 00:28:51 -0700

    Fix: FOUC in prod, move script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import '@/globals.css'
-import { useMemo, type ReactNode } from 'react'
+import type { ReactNode } from 'react'
 import ThemeSelector from '@/ui/ThemeSelector'
 import Footer from '@/ui/Footer'
 import Header from '@/ui/Header'
 import { IBM_Plex_Mono, Montserrat, Work_Sans, Roboto } from 'next/font/google'
 import { ViewTransitions } from 'next-view-transitions'
 import type { Metadata } from 'next/types'
-import { cookies } from 'next/headers'
 import { baseUrl } from '@/lib/config'
 
 const ibmPlexMono = IBM_Plex_Mono({
@@ -31,6 +30,19 @@ export const metadata: Metadata = {
   description: 'Web designer and developer',
 }
 
+const applyThemeScript = `
+        try {
+          const themeCookie = document.cookie.split('; ').find(cookie => cookie.startsWith('theme='))
+          if (themeCookie) {
+            const theme = themeCookie.split('=')[1]
+            document.documentElement.classList.toggle('dark', theme === 'dark');
+          } else {
+            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+            document.documentElement.classList.toggle('dark', prefersDark)
+          }
+        } catch (_) {}
+      `
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <ViewTransitions>
@@ -39,28 +51,19 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         // biome-ignore lint/nursery/useSortedClasses: Bug in Biome
         className={`text-base ${montserrat.variable} ${workSans.variable} ${roboto.variable} ${ibmPlexMono.variable}`}
       >
+        <head>
+          <script
+            // biome-ignore lint/security/noDangerouslySetInnerHtml: Trusted author
+            dangerouslySetInnerHTML={{
+              __html: applyThemeScript,
+            }}
+          />
+        </head>
         <body
           className={
             'mx-auto min-w-full bg-bg p-[1px] font-mono text-text-primary transition-colors duration-[1500]'
           }
         >
-          <script
-            // biome-ignore lint/security/noDangerouslySetInnerHtml: Trusted author
-            dangerouslySetInnerHTML={{
-              __html: `
-            try {
-              const themeCookie = document.cookie.split('; ').find(cookie => cookie.startsWith('theme='))
-              if (themeCookie) {
-                const theme = themeCookie.split('=')[1]
-                document.documentElement.classList.toggle('dark', theme === 'dark');
-              } else {
-                const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-                document.documentElement.classList.toggle('dark', prefersDark)
-              }
-            } catch (_) {}
-          `,
-            }}
-          />
           <div className='m-frame flex h-[calc(100svh-calc(var(--frame)*2)-2px)] flex-col items-center justify-between overflow-hidden border-[1px] border-border'>
             <Header
               links={[
