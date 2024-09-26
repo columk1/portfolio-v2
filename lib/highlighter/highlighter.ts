f53c218be5d877470892ffacbbcbcf11026c2592
Colum Kelly <columk1@users.noreply.github.com>
2024-09-26 15:17:34 -0700

    Convert highlighter to singleton to save memory

--- lib/highlighter/highlighter.ts
+++ lib/highlighter/highlighter.ts
@@ -6,24 +6,35 @@ import tokyoNightLight from './tokyo-night-light.json'
 //   fs.readFileSync('./lib/highlighter/tokyo-night-light.json', 'utf8')
 // )
 
-const highlighter = await createHighlighter({
-  langs: [
-    'html',
-    'css',
-    'javascript',
-    'typescript',
-    'jsx',
-    'tsx',
-    'shell',
-    'json',
-    'python',
-    'markdown',
-    'ansi',
-    'yaml',
-  ],
-  themes: ['tokyo-night'],
-})
+const globalHighlighter = global as typeof globalThis & {
+  highlighterInstance?: ReturnType<typeof createHighlighter>
+}
 
-await highlighter.loadTheme(tokyoNightLight as unknown as ThemeRegistrationResolved)
+if (!globalHighlighter.highlighterInstance) {
+  globalHighlighter.highlighterInstance = (async () => {
+    const highlighter = await createHighlighter({
+      langs: [
+        'html',
+        'css',
+        'javascript',
+        'typescript',
+        'jsx',
+        'tsx',
+        'shell',
+        'json',
+        'python',
+        'markdown',
+        'ansi',
+        'yaml',
+      ],
+      themes: ['tokyo-night'],
+    })
+
+    await highlighter.loadTheme(tokyoNightLight as unknown as ThemeRegistrationResolved)
+    return highlighter
+  })()
+}
+
+const highlighter = await globalHighlighter.highlighterInstance
 
 export default highlighter
