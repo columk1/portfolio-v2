edcf641d1fdf0decb5b93b11d0adf8ba017dbd06
Colum Kelly <columk1@users.noreply.github.com>
2024-09-27 00:48:42 -0700

    Add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+function NotFoundPage() {
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <div className='flex items-center gap-6'>
+        <h1 className='font-bold text-2xl'>404</h1>
+        <div className='h-11 w-[0.5px] bg-text-secondary' />
+        <p className='text-sm'>Page not found</p>
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundPage
