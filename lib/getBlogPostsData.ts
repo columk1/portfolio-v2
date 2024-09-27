42bc0e7078a4884bc4dd2ff79e542ad591edab33
Colum Kelly <columk1@users.noreply.github.com>
2024-09-27 00:08:52 -0700

    Revert "Include drafts in development"
    This reverts commit 2d8b3a7e4087f61a6b70fe7014e759094d84a144.

--- lib/getBlogPostsData.ts
+++ lib/getBlogPostsData.ts
@@ -12,30 +12,20 @@ const isMDXFile = (dirent: Dirent) => !dirent.isDirectory() && dirent.name.endsW
 // const getSlugFromFile = (file: Dirent) => file.name.substring(0, file.name.lastIndexOf('.'))
 
 export async function getBlogPostsData(): Promise<BlogPostData[]> {
-  const includeDrafts = process.env.NODE_ENV === 'development'
   try {
     const contentDir = path.join(process.cwd(), 'content')
-    const draftsDir = path.join(contentDir, 'drafts')
+    console.log(contentDir)
+    const dirents = await readdir(contentDir, { withFileTypes: true })
 
-    const posts = await Promise.all([
-      getPostsFromDirectory(contentDir),
-      includeDrafts ? getPostsFromDirectory(draftsDir, true) : Promise.resolve([]),
-    ])
+    const slugs = dirents
+      .filter(isMDXFile)
+      .map((file) => file.name.substring(0, file.name.lastIndexOf('.')))
 
-    return posts
-      .flat()
-      .sort((a, b) => (new Date(a.metadata.date) > new Date(b.metadata.date) ? -1 : 1))
+    const posts = await Promise.all(slugs.map((slug) => getBlogPostMetadata(slug)))
+
+    return posts.sort((a, b) => (new Date(a.metadata.date) > new Date(b.metadata.date) ? -1 : 1))
   } catch (err) {
     console.log(err)
     return []
   }
-}
-
-async function getPostsFromDirectory(dir: string, isDraft = false): Promise<BlogPostData[]> {
-  const dirents = await readdir(dir, { withFileTypes: true })
-  const slugs = dirents
-    .filter(isMDXFile)
-    .map((file) => file.name.substring(0, file.name.lastIndexOf('.')))
-
-  return Promise.all(slugs.map((slug) => getBlogPostMetadata(slug, isDraft)))
 }
