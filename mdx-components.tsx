3080770598247bd72b232c0d8820131fbc921aa8
Colum Kelly <columk1@users.noreply.github.com>
2024-10-04 10:19:44 -0700

    Create Image component with caption, update posts

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,7 +4,7 @@ import type { MDXComponents } from 'mdx/types'
 import { stripCodeTags } from './lib/utils/stripCodeTags'
 import { addCopyButton } from './lib/utils/addCopyButton'
 import highlighter from './lib/highlighter/highlighter'
-import Image from 'next/image'
+import NextImage from 'next/image'
 
 type HeadingProps = ComponentPropsWithoutRef<'h1'>
 type ParagraphProps = ComponentPropsWithoutRef<'p'>
@@ -12,23 +12,22 @@ type ListProps = ComponentPropsWithoutRef<'ul'>
 type ListItemProps = ComponentPropsWithoutRef<'li'>
 type AnchorProps = ComponentPropsWithoutRef<'a'>
 type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>
-type ImageProps = ComponentPropsWithoutRef<'img'>
+type ImageProps = ComponentPropsWithoutRef<'img'> & { caption?: string }
 
 const components: MDXComponents = {
   h1: (props: HeadingProps) => (
     <h1 className='fade-in mb-0 pt-12 font-light font-sans text-4xl' {...props} />
   ),
-  h2: (props: HeadingProps) => <h2 className='mt-8 mb-3 font-medium text-2xl' {...props} />,
+  h2: (props: HeadingProps) => (
+    <h2 className='mt-8 mb-3 font-light text-3xl text-link' {...props} />
+  ),
   h3: (props: HeadingProps) => <h3 className='mt-8 mb-3 font-medium text-xl' {...props} />,
   h4: (props: HeadingProps) => <h4 className='font-normal' {...props} />,
-  p: (props: ParagraphProps) => <p className='mb-6' {...props} />,
+  p: (props: ParagraphProps) => <p className='mb-4' {...props} />,
   ol: (props: ListProps) => <ol className='list-decimal space-y-1 pl-5' {...props} />,
   ul: (props: ListProps) => <ul className='list-disc space-y-0 pl-5' {...props} />,
   li: (props: ListItemProps) => <li className='pl-1' {...props} />,
-  // Use this as the image caption until an image component is created
-  em: (props: ComponentPropsWithoutRef<'em'>) => (
-    <em className='mt-4 block text-center font-normal text-sm' {...props} />
-  ),
+  em: (props: ComponentPropsWithoutRef<'em'>) => <em className='italic' {...props} />,
   strong: (props: ComponentPropsWithoutRef<'strong'>) => (
     <strong className='font-medium' {...props} />
   ),
@@ -91,21 +90,37 @@ const components: MDXComponents = {
   blockquote: (props: BlockquoteProps) => (
     <blockquote className='ml-2 border-l-4 bg-neutral-800 pl-5 font-normal italic' {...props} />
   ),
-  img: (props: ImageProps) => (
-    <Image
-      className='mx-auto block max-w-[min(100%,400px)] rounded-md bg-bg-light xl:max-w-100%'
-      src={props.src as string}
-      alt={props.alt || ''}
-      {...props}
-      width={(props.width as number) || 800}
-      height={(props.height as number) || 800}
-    />
-  ),
+  img: (props: ImageProps) => Image(props),
 }
 
 export function useMDXComponents(otherComponents: MDXComponents): MDXComponents {
   return {
+    Image,
     ...otherComponents,
     ...components,
   }
+}
+
+function Image(props: ImageProps) {
+  const { caption, src, alt, ...rest } = props
+  const parsedWidth = Number(props.width)
+  const width = !Number.isNaN(parsedWidth) ? parsedWidth : 800
+
+  return (
+    <figure className='my-6 flex flex-col gap-4'>
+      <NextImage
+        className='mx-auto block h-auto max-w-full rounded-md bg-bg-light xl:max-w-100%'
+        src={src as string}
+        alt={alt || ''}
+        {...rest}
+        width={width} // Use incoming width or default to 800
+        height={800}
+      />
+      {caption && (
+        <figcaption className='block text-center font-detail text-text-primary text-xs'>
+          {caption}
+        </figcaption>
+      )}
+    </figure>
+  )
 }
